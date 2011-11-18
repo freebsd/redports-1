a321343cf10e83c5c79a39e9ac9f7a90ad70948d
decke <decke@2dc3c0fd-c7c2-1e80-8f18-47a4e6f48fb9>
2011-11-18 14:33:34 +0000

    rptinderbox: - Whitespace cleanup

--- rptinderbox/rptinderbox_buildhook.sh
+++ rptinderbox/rptinderbox_buildhook.sh
@@ -16,46 +16,46 @@ LOCK=/tmp/rptinderbox/${BUILD}/.lock
 FINISHED=/tmp/rptinderbox/${BUILD}/.finished
 
 compress_wrkdir () {
-  mkdir -p "${PB}/wrkdirs/${BUILD}" && \
-  cd ${PB}/${BUILD}/a/ports/${PORTDIR} && objdir=$(make -V WRKDIR) && \
-  objdir=`echo ${objdir} | sed "s,${PB}/${BUILD}/,${PB}/${BUILD}/work/,"` && \
-  tar cfjC ${PB}/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz ${objdir}/.. work && \
-  echo "WRKDIR=\"/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz\"" >> ${FINISHED}
-  return 0
+    mkdir -p "${PB}/wrkdirs/${BUILD}" && \
+    cd ${PB}/${BUILD}/a/ports/${PORTDIR} && objdir=$(make -V WRKDIR) && \
+    objdir=`echo ${objdir} | sed "s,${PB}/${BUILD}/,${PB}/${BUILD}/work/,"` && \
+    tar cfjC ${PB}/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz ${objdir}/.. work && \
+    echo "WRKDIR=\"/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz\"" >> ${FINISHED}
+    return 0
 }
 
 if [ -f "${LOCK}" ]; then
-  . ${LOCK}
+    . ${LOCK}
 
-  if [ "${PORTDIR}" = "${PORT}" -o "${FAIL_REASON}" != "" ]; then
-    touch ${FINISHED}
+    if [ "${PORTDIR}" = "${PORT}" -o "${FAIL_REASON}" != "" ]; then
+        touch ${FINISHED}
 
-    echo "BUILDSTATUS=\"${STATUS}\"" >> ${FINISHED}
-    echo "PACKAGE_NAME=\"${PACKAGE_NAME}\"" >> ${FINISHED}
+        echo "BUILDSTATUS=\"${STATUS}\"" >> ${FINISHED}
+        echo "PACKAGE_NAME=\"${PACKAGE_NAME}\"" >> ${FINISHED}
 
-    if [ "${PORTDIR}" = "${PORT}" ]; then
-      echo "FAIL_REASON=\"${FAIL_REASON}\"" >> ${FINISHED}
-    else
-      echo "FAIL_REASON=\"depend (${FAIL_REASON} in ${PORTDIR})\"" >> ${FINISHED}
+        if [ "${PORTDIR}" = "${PORT}" ]; then
+            echo "FAIL_REASON=\"${FAIL_REASON}\"" >> ${FINISHED}
+        else
+            echo "FAIL_REASON=\"depend (${FAIL_REASON} in ${PORTDIR})\"" >> ${FINISHED}
+        fi
+
+        curl -s "${FINISHURL}" >/dev/null
     fi
-
-    curl -s "${FINISHURL}" >/dev/null
-  fi
 fi
 
 if [ "${STATUS}" = "SUCCESS" -o "${STATUS}" = "LEFTOVERS" ]; then
-  if [ -f "${LOCK}" -a "${PORTDIR}" = "${PORT}" ]; then
-    echo "BUILDLOG=\"/logs/${BUILD}/${PACKAGE_NAME}.log\"" >> ${FINISHED}
-  fi
-  if [ -n "${DELETE_OLD}" ]; then
-    rm ${PB}/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz
-  fi
-  if [ -n "${COMPRESS_ALL}" ]; then
-    compress_wrkdir
-  fi
+    if [ -f "${LOCK}" -a "${PORTDIR}" = "${PORT}" ]; then
+        echo "BUILDLOG=\"/logs/${BUILD}/${PACKAGE_NAME}.log\"" >> ${FINISHED}
+    fi
+    if [ -n "${DELETE_OLD}" ]; then
+        rm ${PB}/wrkdirs/${BUILD}/${PACKAGE_NAME}.tbz
+    fi
+    if [ -n "${COMPRESS_ALL}" ]; then
+        compress_wrkdir
+    fi
 elif [ "${STATUS}" != "DUD" ]; then
-  if [ -f "${LOCK}" ]; then
-    echo "BUILDLOG=\"/errors/${BUILD}/${PACKAGE_NAME}.log\"" >> ${FINISHED}
-  fi
-  compress_wrkdir
+    if [ -f "${LOCK}" ]; then
+        echo "BUILDLOG=\"/errors/${BUILD}/${PACKAGE_NAME}.log\"" >> ${FINISHED}
+    fi
+    compress_wrkdir
 fi
