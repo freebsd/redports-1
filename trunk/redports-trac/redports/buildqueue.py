4b5b893b7931a78678f53e8d775adb510449396c
decke <decke@2dc3c0fd-c7c2-1e80-8f18-47a4e6f48fb9>
2011-11-18 14:12:07 +0000

    redports-trac: - Whitespace cleanup - Remove useless comments

--- trunk/redports-trac/redports/buildqueue.py
+++ trunk/redports-trac/redports/buildqueue.py
@@ -13,53 +13,26 @@ import re
 from model import Port, PortsQueueIterator, AllBuildgroupsIterator
 
 class BuildqueuePanel(Component):
-    """ Pages for adding/editing contacts. """
     implements(INavigationContributor, ITemplateProvider, IRequestHandler, IPermissionRequestor)
 
-    #   INavigationContributor methods
     def get_active_navigation_item(self, req):
-        """This method is only called for the `IRequestHandler` processing the
-        request.
-        
-        It should return the name of the navigation item that should be
-        highlighted as active/current.
-        """
         return 'buildqueue'
+
     def get_navigation_items(self, req):
-        """Should return an iterable object over the list of navigation items to
-        add, each being a tuple in the form (category, name, text).
-        """
         if 'BUILDQUEUE_VIEW' in req.perm('buildqueue'):
             yield('mainnav', 'buildqueue', tag.a(_('Your Builds'), href=req.href.buildqueue()))
 
-    #   ITemplateProvider
     def get_htdocs_dirs(self):
         return [('redports', resource_filename('redports', 'htdocs'))]
+
     def get_templates_dirs(self):
-        """Return a list of directories containing the provided template
-        files.
-        """
         return [resource_filename('redports', 'templates')]
 
-    #   IRequestHandler methods
     def match_request(self, req):
-        """Return whether the handler wants to process the given request."""
         if re.match(r'^/buildqueue', req.path_info):
             return True
+
     def process_request(self, req):
-        """Process the request. For ClearSilver, return a (template_name,
-        content_type) tuple, where `template` is the ClearSilver template to use
-        (either a `neo_cs.CS` object, or the file name of the template), and
-        `content_type` is the MIME type of the content. For Genshi, return a
-        (template_name, data, content_type) tuple, where `data` is a dictionary
-        of substitutions for the template.
-
-        For both templating systems, "text/html" is assumed if `content_type` is
-        `None`.
-
-        Note that if template processing should not occur, this method can
-        simply send the response itself and not return anything.
-        """
         req.perm('redports').assert_permission('BUILDQUEUE_VIEW')
 
         repopath = RepositoryManager(self.env).get_repository(None).get_path_url(req.authname, 1)
@@ -75,6 +48,7 @@ class BuildqueuePanel(Component):
             port.revision = req.args.get('revision')
             port.portname = req.args.get('portname')
             port.group = req.args.get('group')
+
             if port.addPort():
                 add_notice(req, 'New builds for port %s have been scheduled', req.args.get('portname'))
             else:
@@ -97,13 +71,6 @@ class BuildqueuePanel(Component):
                 'revision': repos.youngest_rev,
                 'authname': req.authname
             }, None)
-    #   IPermissionRequest methods
+
     def get_permission_actions(self):
-        """Return a list of actions defined by this component.
-        
-        The items in the list may either be simple strings, or
-        `(string, sequence)` tuples. The latter are considered to be "meta
-        permissions" that group several simple actions under one name for
-        convenience.
-        """
         return ['BUILDQUEUE_VIEW']
